b7978f29d572657fdc005e2baad3397490216ced
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2022-07-12 16:15:03 -0700

    Initial commit

---
+++ src/lib/utils/canvas.js
@@ -0,0 +1,50 @@
+export const PI = Math.PI;
+export const TWOPI = Math.PI * 2;
+
+export const cream = '#F9F6EE';
+export const fog = '#EFEFEB';
+export const green = '#0ABB62';
+export const colors = [
+	['rgba(112, 184, 255, 0.35)', 'rgba(112, 184, 255, 0.0)'],
+	['rgba(57, 208, 133, 0.25)', 'rgba(57, 208, 133, 0.0)']
+];
+export const createGrid = (a, b) => Math.floor(a / b) + 1;
+
+function createVector(_x, _y) {
+	const vectorObj = { x: _x, y: _y };
+	return vectorObj;
+}
+
+function set(vect, val1, val2) {
+	vect.x = val1;
+	vect.y = val2 ? val2 : val1;
+	return vect;
+}
+
+export class Circle {
+	constructor(c, x = 0, y = 0) {
+		this.pos = createVector(x, y);
+		this.vel = createVector(0, 0);
+		this.c = c;
+	}
+	update(vel1, vel2) {
+		set(this.pos, vel1, vel2);
+		this.t += this.inc;
+	}
+	show(ctx, rad) {
+		const gradient = ctx.createRadialGradient(
+			this.pos.x,
+			this.pos.y,
+			0,
+			this.pos.x,
+			this.pos.y,
+			rad
+		);
+		gradient.addColorStop(0, this.c[0]);
+		gradient.addColorStop(1, this.c[1]);
+		ctx.fillStyle = gradient;
+		ctx.beginPath();
+		ctx.arc(this.pos.x, this.pos.y, rad, 0, TWOPI, false);
+		ctx.fill();
+	}
+}
---
+++ src/routes/index.svelte
@@ -0,0 +1,136 @@
+<script>
+	import { onMount } from 'svelte';
+	import SimplexNoise from 'simplex-noise';
+	import { PI, cream, fog, green, colors, createGrid, Circle } from '$lib/utils/canvas';
+
+	let canvas, columns, container, ctx, cR, field, rows, scale, simplex, size, wX, wY;
+	let time = 0;
+	const inc = 0.003;
+	let circles = [];
+
+	function setup() {
+		scale = window.devicePixelRatio;
+		wX = container.offsetWidth;
+		wY = container.offsetHeight;
+		size = wX < 600 ? 16 : 20;
+		canvas.style.width = wX + 'px';
+		canvas.style.height = wY + 'px';
+		canvas.width = wX * scale;
+		canvas.height = wY * scale;
+		ctx.scale(scale, scale);
+		columns = createGrid(wX, size);
+		rows = createGrid(wY, size);
+		initField();
+		cR = (wX / 3) * 2;
+	}
+
+	function draw() {
+		window.requestAnimationFrame(draw);
+		calculateField();
+		clear();
+		drawField();
+		drawCircles();
+		time += inc;
+	}
+
+	// setup the grid of vectors
+	function initField() {
+		field = new Array(columns);
+		for (let x = 0; x < columns; x++) {
+			field[x] = new Array(rows);
+			for (let y = 0; y < rows; y++) {
+				field[x][y] = [0, 0];
+			}
+		}
+	}
+
+	// run the animation
+	function drawField() {
+		for (let x = 0; x < columns; x++) {
+			for (let y = 0; y < rows; y++) {
+				const angle = field[x][y][0];
+				const length = field[x][y][1];
+				ctx.save();
+				ctx.translate(x * size, y * size);
+				ctx.rotate(angle);
+				ctx.strokeStyle = green;
+				ctx.beginPath();
+				ctx.moveTo(0, 0);
+				ctx.lineTo(0, size * length);
+				ctx.stroke();
+				ctx.restore();
+			}
+		}
+	}
+
+	// clear the canvas at the beginning of each frame
+	function clear() {
+		ctx.fillStyle = fog;
+		ctx.fillRect(0, 0, wX, wY);
+	}
+
+	// calculate each frame of the animation
+	function calculateField() {
+		for (let x = 0; x < columns; x++) {
+			for (let y = 0; y < rows; y++) {
+				const angle = simplex.noise3D(x / 50, y / 50, time) * (PI * 2);
+				const length = simplex.noise3D(x / 100 + 40000, y / 100 + 40000, time);
+				field[x][y][0] = angle;
+				field[x][y][1] = length;
+			}
+		}
+	}
+
+	function createCircles() {
+		for (let i = 0; i < 2; i++) {
+			circles.push(new Circle(colors[i]));
+		}
+	}
+
+	function drawCircles() {
+		const sine = [Math.sin(time) * (wX / 2) + wX / 2, Math.sin(time + PI) * (wX / 2) + wX / 2];
+		const cosine = [Math.cos(time) * (wX / 2) + wX / 2, Math.cos(time + PI) * (wX / 2) + wX / 2];
+		for (const [i, circle] of circles.entries()) {
+			circle.update(sine[i], cosine[i]);
+			circle.show(ctx, cR);
+		}
+	}
+
+	onMount(() => {
+		simplex = new SimplexNoise();
+		ctx = canvas.getContext('2d');
+		setup();
+		createCircles();
+		draw();
+	});
+</script>
+
+<section class="hero">
+	<div bind:this={container} class="container">
+		<canvas bind:this={canvas} id="canvas" width="400" height="400" />
+	</div>
+</section>
+
+<style>
+	section.hero {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		min-height: 100vh;
+		background: #f9f6ee;
+	}
+
+	div.container {
+		width: 80vh;
+		height: 80vh;
+		aspect-ratio: 1/1;
+		border-radius: 50%;
+		overflow: hidden;
+		/* background: green; */
+	}
+
+	canvas {
+		display: block;
+	}
+</style>
